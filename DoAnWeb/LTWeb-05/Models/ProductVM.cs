24cb3f2873f094d843f042b7b8a59cac9e9fb826
Nghi <chauphunghi@gmail.com>
2017-06-22 12:30:55 +0700

    Profile va thay doi Profile  , thay doi Pass

---
+++ DoAnWeb/LTWeb-05/Models/ProductVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LTWeb_05.Models
+{
+    public class ProductVM
+    {
+        public int ProID { get; set; }
+        public string ProName { get; set; }
+        public string TinyDes { get; set; }
+        public string FullDes { get; set; }
+        public decimal Price { get; set; }
+        public int CatID { get; set; }
+        public int Quantity { get; set; }
+        public Nullable<decimal> MaxPrice { get; set; }
+        public Nullable<System.DateTime> Expried { get; set; }
+        public Nullable<int> f_IDPost { get; set; }
+    }
+}
