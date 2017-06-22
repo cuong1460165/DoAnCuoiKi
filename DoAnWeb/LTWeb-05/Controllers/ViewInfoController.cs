f0d32ea24cbf66950a138d238e76386d9b375f19
Mau <smau1906@gmail.com>
2017-06-22 13:13:54 +0700

    Danh sach yeu thich

---
+++ DoAnWeb/LTWeb-05/Controllers/ViewInfoController.cs
@@ -0,0 +1,39 @@
+﻿using LTWeb_05.Helpers;
+using LTWeb_05.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTWeb_05.Controllers
+{
+    public class ViewInfoController : Controller
+    {
+        //
+        // GET: /ViewInfo/
+        public ActionResult WatchList()
+        {
+            using (var ctx = new QLBHEntities())
+            {
+                var iduser = CurrentContext.GetCurUser().f_ID;
+                var list = (from p in ctx.Products
+                            join f in ctx.FavoriteProducts
+                            on p.ProID equals f.ProID
+                            where f.f_IDLike == iduser
+                            select new ProductVM
+                            {
+                                ProID = p.ProID,
+                                ProName = p.ProName,
+                                TinyDes = p.TinyDes,
+                                FullDes = p.FullDes,
+                                Price = p.Price,
+                                CatID = p.CatID,
+                                MaxPrice = p.MaxPrice,
+                                Expried = p.Expried,
+                            }).ToList();
+                return View(list);
+            }
+        }
+	}
+}
---
+++ DoAnWeb/LTWeb-05/Helpers/Cart.cs
@@ -0,0 +1,26 @@
+﻿using LTWeb_05.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LTWeb_05.Helpers
+{
+    public class Cart
+    {
+        public List<CartItem> Items { get; set; }
+        public Cart()
+        {
+            this.Items = new List<CartItem>();
+        }
+        public void AddItem(CartItem item)
+        {
+            this.Items.Add(item);
+        }
+    }
+     public class CartItem
+        {
+            public Product product { get; set; }
+            public int Quantity { get; set; }
+        }
+}
