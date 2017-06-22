8c1cae92d4dde51f0600430da48cb8bc21c1b1b4
Mau <smau1906@gmail.com>
2017-06-22 11:14:14 +0700

    Danh muc san pham

---
+++ DoAnWeb/LTWeb-05/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LTWeb_05.Models;
+namespace LTWeb_05.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //
+        // GET: /Category/
+        public ActionResult List()
+        {
+
+            using (var ctx = new QLBHEntities())
+            {
+                var list = ctx.Categories.ToList();
+                return PartialView("ListPartial",list);
+            }
+          
+
+        }
+	}
+}
--- DoAnWeb/LTWeb-05/Controllers/HomeController.cs
+++ DoAnWeb/LTWeb-05/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LTWeb_05.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,9 +11,31 @@ namespace LTWeb_05.Controllers
     {
         //
         // GET: /Home/
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-                return View();
+            using (var ctx = new QLBHEntities())
+            {
+                int n = ctx.Products.Count();
+                int recordsPerPage = 6;
+                int nPages = n / recordsPerPage;
+                int m = n % recordsPerPage;
+                if (m > 0)
+                {
+                    nPages++;
+                }
+                ViewBag.Pages = nPages;
+                ViewBag.CurPage = page;
+                var list = ctx.Products
+                    .OrderBy(p => p.ProID)
+                    .Skip((page - 1) * recordsPerPage)
+                    .Take(recordsPerPage)
+                    .ToList();
+
+                //var list = ctx.Products
+                //    .Where(p => id == p.CatID)
+                //    .ToList();
+                return View(list);
+            }
         }
 	}
 }
