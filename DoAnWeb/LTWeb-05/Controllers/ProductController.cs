3630d53fd360510167b8db3598fc7da715aa2eb3
Nghi <chauphunghi@gmail.com>
2017-06-22 11:57:48 +0700

    Xem chi tiet san pham

--- DoAnWeb/LTWeb-05/Controllers/ProductController.cs
+++ DoAnWeb/LTWeb-05/Controllers/ProductController.cs
@@ -10,35 +10,35 @@ namespace LTWeb_05.Controllers
     {
         public ActionResult Product(int page = 1)
         {
-            using (var ctx = new QLBHEntities())
-            {
-                int n = ctx.Products.Count();
-                int recordsPerPage = 6;
-                int nPages = n / recordsPerPage;
-                int m = n % recordsPerPage;
-                if (m > 0)
-                {
-                    nPages++;
-                }
-                ViewBag.Pages = nPages;
-                ViewBag.CurPage = page;
-                var list = ctx.Products
-                    .OrderBy(p => p.ProID)
-                    .Skip((page - 1) * recordsPerPage)
-                    .Take(recordsPerPage)
-                    .ToList();
+           using (var ctx = new QLBHEntities())
+           {
+               int n = ctx.Products.Count();
+               int recordsPerPage = 6;
+               int nPages = n / recordsPerPage;
+               int m = n % recordsPerPage;
+               if (m > 0)
+               {
+                   nPages++;
+               }
+               ViewBag.Pages = nPages;
+               ViewBag.CurPage = page;
+               var list = ctx.Products
+                   .OrderBy(p => p.ProID)
+                   .Skip((page - 1) * recordsPerPage)
+                   .Take(recordsPerPage)
+                   .ToList();
 
-                //var list = ctx.Products
-                //    .Where(p => id == p.CatID)
-                //    .ToList();
-                return View(list);
-            }
+               //var list = ctx.Products
+               //    .Where(p => id == p.CatID)
+               //    .ToList();
+               return View(list);
+           }
         }
         //
         // GET: /Product/ByCat
-        public ActionResult ByCat(int? id, int page = 1)
+        public ActionResult ByCat(int ?id, int page = 1)
         {
-            if (id.HasValue == false)
+            if(id.HasValue == false)
             {
                 return RedirectToAction("Index", "Home");
             }
@@ -50,7 +50,7 @@ namespace LTWeb_05.Controllers
                 int recordsPerPage = 6;
                 int nPages = n / recordsPerPage;
                 int m = n % recordsPerPage;
-                if (m > 0)
+                if(m > 0)
                 {
                     nPages++;
                 }
@@ -59,7 +59,7 @@ namespace LTWeb_05.Controllers
                 var list = ctx.Products
                     .Where(p => p.CatID == id)
                     .OrderBy(p => p.ProID)
-                    .Skip((page - 1) * recordsPerPage)
+                    .Skip((page-1)*recordsPerPage)
                     .Take(recordsPerPage)
                     .ToList();
 
@@ -69,5 +69,21 @@ namespace LTWeb_05.Controllers
                 return View(list);
             }
         }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id.HasValue == false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            using (var ctx = new QLBHEntities())
+            {
+                var model = ctx.Products
+                    .Where(p => p.ProID == id)
+                    .FirstOrDefault();
+                return View(model);
+            }
+        }
+      
     }
 }
