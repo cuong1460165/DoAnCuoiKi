1bd3c14154f636821d4cc13ec78de7cda57983dc
Nghi <chauphunghi@gmail.com>
2017-06-22 11:27:58 +0700

    Load san pham trang chu

---
+++ DoAnWeb/LTWeb-05/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LTWeb_05.Models;
+namespace LTWeb_05.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Product(int page = 1)
+        {
+            using (var ctx = new QLBHEntities())
+            {
+                int n = ctx.Products.Count();
+                int recordsPerPage = 6;
+                int nPages = n / recordsPerPage;
+                int m = n % recordsPerPage;
+                if (m > 0)
+                {
+                    nPages++;
+                }
+                ViewBag.Pages = nPages;
+                ViewBag.CurPage = page;
+                var list = ctx.Products
+                    .OrderBy(p => p.ProID)
+                    .Skip((page - 1) * recordsPerPage)
+                    .Take(recordsPerPage)
+                    .ToList();
+
+                //var list = ctx.Products
+                //    .Where(p => id == p.CatID)
+                //    .ToList();
+                return View(list);
+            }
+        }
+    }
+}
+      
